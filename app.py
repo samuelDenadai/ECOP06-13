a7b9f34acc99f1044dc77d465dd5381e091a97d7
samuelDenadai <146841518+samuelDenadai@users.noreply.github.com>
2023-10-23 14:14:36 +0000

    Atualizado

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd
 
+st.set_page_config(
+    'Samuel - ECOP06',
+    'https://unifei.edu.br/wp-content/themes/twentytwelve-child/img/cabecalho/logo-unifei-oficial.png')
+
+st.title('Pagina Teste de ECOP06')
+
+esportes - pd.read_csv(
+    'https://github.com/MainakRepositor/Datasets/raw/master/GeneralEsportData.csv',
+    encoding='latin-1')
+
+st.dataframe(esportes)
